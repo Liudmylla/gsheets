553a45d3385bce11eab6051e6b2eaf94ed70abc8
Liudmylla <liudmyla12@yahoo.fr>
2024-04-06 23:22:54 +0200

    delete log

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ function App() {
   useEffect(() => {
     fetch('https://script.google.com/macros/s/AKfycbz2Lvzp_g0AeUElg6SbbQWiPirtRhL4CCCqkVYgVoopC9jrvQRoyBWIfu1jggJCfHEG/exec')
       .then(response => response.json())
-      .then(data => {
-        console.log(data); // Log the response
-        setData(data);
-      })
+      .then(data =>setData(data))
       .catch(error => console.error('Error fetching data:', error));
   }, []);
 
